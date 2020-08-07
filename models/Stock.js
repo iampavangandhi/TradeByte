dd88208adb8a6397f675b2a7e5f39ed81b4f59c8
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2020-08-08 00:21:44 +0530

    Added Stock Schema for fix/#9

--- models/Stock.js
+++ models/Stock.js
@@ -1,1 +1,23 @@
 // Stock Model
+const mongoose = require('mongoose')
+
+const StockSchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+    stockPrice: {
+        type: Float,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref = 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model(Stock, StockSchema)
