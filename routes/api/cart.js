2eff63408cec36b76cf38f50fa1899dfdd0de559
Pavan Gandhi <pavangandhi100@gmail.com>
2020-08-10 21:19:36 +0530

    Fixed Issues

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -1,54 +1,54 @@
 // Cart Routes
-const express = require('express')
-const router = express.Router()
-const { ensureAuth } = require('../../middleware/auth')
-const User = require('../../models/User')
+const express = require("express");
+const router = express.Router();
+const { ensureAuth } = require("../../middleware/auth");
+const User = require("../../models/User");
 
 // TODO
 // Stocks Cart Buy/Sell
 
+// TODO
+// Empty Cart Route
 
 // @desc    To buy
 // @route   GET /cart
-router.get('/', ensureAuth, async (req, res) => {
-    console.log(req.user)
-    res.render('cart')
-})
-
+router.get("/:symbol", ensureAuth, async (req, res) => {
+  const symbol = req.params.symbol;
+  res.status(200).render("cart", { layout: "layouts/app", symbol });
+});
 
 // @desc    To buy
 // @route   POST /cart/buy
-router.post('/buy', ensureAuth, async (req, res) => {
-    let data = req.body
-    let user = req.user
-    let companySymbol = req.body.companySymbol
-    let stockPrice = req.body.stockPrice
-    let noOfStock = req.body.noOfStock
-    let totalAmount = stockPrice * noOfStock
+router.post("/buy", ensureAuth, async (req, res) => {
+  let data = req.body;
+  let user = req.user;
+  let companySymbol = req.body.companySymbol;
+  let stockPrice = req.body.stockPrice;
+  let noOfStock = req.body.noOfStock;
+  let totalAmount = stockPrice * noOfStock;
 
-    try {
-        console.log(req.user)
-        console.log(req.body)
-        // req.body.user = req.user.id
-        if (totalAmount > req.user.balance) {
-            let ExtraBalance = totalAmount - req.user.balance
-            res.render('transaction', {
-                ExtraBalance,
-                message: "Insufficent Balance",
-            })
-        } else {
-            res.render('transaction', {
-                data,
-                user,
-                totalAmount,
-                message: "Order Review",
-            })
-        }
-        
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
+  try {
+    console.log(req.user);
+    console.log(req.body);
+    // req.body.user = req.user.id
+    if (totalAmount > req.user.balance) {
+      let ExtraBalance = totalAmount - req.user.balance;
+      res.render("transaction", {
+        ExtraBalance,
+        message: "Insufficent Balance",
+      });
+    } else {
+      res.render("transaction", {
+        data,
+        user,
+        totalAmount,
+        message: "Order Review",
+      });
     }
-})
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
 module.exports = router;
--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -1,32 +1,34 @@
 // Cart Routes
-const express = require('express')
-const router = express.Router()
-const { ensureAuth } = require('../../middleware/auth')
-const User = require('../../models/User')
-
+const express = require("express");
+const router = express.Router();
+const { ensureAuth } = require("../../middleware/auth");
+const User = require("../../models/User");
 
 // @desc    To buy
 // @route   POST transaction/confirm
-router.put('/confirm', ensureAuth, async (req, res) => {
-    try {
-        balance = req.user.balance - req.body.totalAmount
+router.put("/confirm", ensureAuth, async (req, res) => {
+  try {
+    balance = req.user.balance - req.body.totalAmount;
 
-        req.body.user = req.user.id
-        // Deducting Amount and updating it
-        await User.findOneAndUpdate({_id : req.user.id}, {balance: balance})
-        
-        // Stock is not appended, it overwirtes on previous one
-        await User.findOneAndUpdate( {_id: req.user.id}, {stock: req.body}, {
-            new: true,                                  // it will create a new one, if it doesn't exist
-            runValidators: true                         // it check weather the fields are valid or not
-        })
-        console.log(req.user.stock)
-        res.redirect('../cart')
-        
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
-    }
-})
+    req.body.user = req.user.id;
+    // Deducting Amount and updating it
+    await User.findOneAndUpdate({ _id: req.user.id }, { balance: balance });
+
+    // Stock is not appended, it overwirtes on previous one
+    await User.findOneAndUpdate(
+      { _id: req.user.id },
+      { stock: req.body },
+      {
+        new: true, // it will create a new one, if it doesn't exist
+        runValidators: true, // it check weather the fields are valid or not
+      }
+    );
+    console.log(req.user.stock);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
 module.exports = router;
