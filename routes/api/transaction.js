0f8e5c6239d9d2949fee8750b93be5249901b91a
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2020-08-10 20:36:33 +0530

    Added Buy feature but having storing stock

--- routes/api/transaction.js
+++ routes/api/transaction.js
@@ -12,8 +12,10 @@ router.put('/confirm', ensureAuth, async (req, res) => {
         balance = req.user.balance - req.body.totalAmount
 
         req.body.user = req.user.id
+        // Deducting Amount and updating it
         await User.findOneAndUpdate({_id : req.user.id}, {balance: balance})
         
+        // Stock is not appended, it overwirtes on previous one
         await User.findOneAndUpdate( {_id: req.user.id}, {stock: req.body}, {
             new: true,                                  // it will create a new one, if it doesn't exist
             runValidators: true                         // it check weather the fields are valid or not
