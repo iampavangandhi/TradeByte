10f03b1be0f8586a2cddf01e77fda18e42f66bdc
garimasingh22 <44470092+garimasingh22@users.noreply.github.com>
2020-08-16 23:54:50 +0530

    ui part done pagination not implemented

--- routes/api/market.js
+++ routes/api/market.js
@@ -4,9 +4,23 @@ const express = require("express");
 const router = express.Router();
 const alpha = require("alphavantage")({ key: process.env.ALPHA_VANTAGE_KEY });
 
-const jsonData = require("../../config/data-compact.json");
-const jsonData1 = require("../../config/data-total.json");
+var jsonData = [];
+const jsonData1 = require("../../config/data-compact.json");
+const jsonData2 = require("../../config/data-total.json");
 const { ensureAuth, ensureGuest } = require("../../middleware/auth");
+
+const dataCnt1 = jsonData1.length;
+const dataCnt2 = jsonData2.length;
+const dataLast1 = dataCnt1 + 1;
+const dataStart2 = dataCnt1 + 1;
+
+jsonData.push({ data1: dataCnt1, data2: dataCnt2, dataLast1: dataLast1, dataStart2: dataStart2 })
+for (var i = 0; i < dataCnt1; i++) {
+    jsonData.push(jsonData1[i]);
+};
+for (var i = 0; i < dataCnt2; i++) {
+    jsonData.push(jsonData2[i]);
+};
 
 // TODO
 // Implement Stocks Search
@@ -18,7 +32,7 @@ const { ensureAuth, ensureGuest } = require("../../middleware/auth");
 router.get("/", ensureAuth, async(req, res) => {
     res
         .status(200)
-        .render("market", { layout: "layouts/app", jsonData, jsonData1, href: "/market" });
+        .render("market", { layout: "layouts/app", jsonData, href: "/market" });
 });
 
 
