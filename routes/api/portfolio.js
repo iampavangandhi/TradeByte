e95ee1f3295ceb3ccfacc90dfecbd430def4cd5c
garimasingh22 <44470092+garimasingh22@users.noreply.github.com>
2020-08-24 10:10:57 +0530

    #44

--- routes/api/portfolio.js
+++ routes/api/portfolio.js
@@ -4,14 +4,16 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../../middleware/auth");
 
+const totalData = require("../../config/data-total.json");
+
 // @desc     Portfolio page
 // @route    GET /portfolio
 // @access   Private
 router.get("/", ensureAuth, (req, res) => {
-  let avatar = req.user.image;
-  res
-    .status(200)
-    .render("portfolio", { layout: "layouts/app", avatar, href: "/portfolio" });
+    let avatar = req.user.image;
+    res
+        .status(200)
+        .render("portfolio", { layout: "layouts/app", avatar, totalData, href: "/portfolio" });
 });
 
 module.exports = router;
