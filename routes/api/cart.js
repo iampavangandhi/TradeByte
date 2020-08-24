32ae0ada27b5adbc484e67454660e42be6b589f4
fabcodingzest <48706652+fabcodingzest@users.noreply.github.com>
2020-08-24 18:00:51 +0530

    Bug Fixes in Cart.js

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -37,6 +37,7 @@ router.post("/buy", ensureAuth, async (req, res) => {
   const symbol = req.body.companySymbol;
   const { latestPrice } = await getPrice(symbol);
   const noOfStock = req.body.noOfStock;
+  const stockPrice = req.body.stockPrice;
   const totalAmount = parseFloat(latestPrice * noOfStock).toFixed(4);
 
   const data = {
