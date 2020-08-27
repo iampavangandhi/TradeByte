e33eff8def9dfe49981eae5c520f7b36ebfbfd98
garimasingh22 <44470092+garimasingh22@users.noreply.github.com>
2020-08-27 21:09:34 +0530

    fixed bugs

--- routes/api/edit.js
+++ routes/api/edit.js
@@ -11,14 +11,13 @@ const { last } = require('lodash');
 
 // @desc    Show edit page
 // @route   GET /edit/:id
-router.get("/:id", ensureAuth, (req, res) => {
+router.get("/", ensureAuth, (req, res) => {
     let avatar = req.user.image;
-    let curruser = req.user;
+
     let firstName = req.user.firstName;
     let lastName = req.user.lastName;
     let email = req.user.email;
-    let googleId = req.user.googleId;
-    let _id = req.user._id;
+
 
     console.log(avatar);
 
@@ -29,27 +28,23 @@ router.get("/:id", ensureAuth, (req, res) => {
         firstName,
         lastName,
         email,
-        googleId,
-        _id,
         href: "/edit",
     });
 });
 
-router.put("/:id", ensureAuth, async(req, res) => {
+router.put("/", ensureAuth, async(req, res) => {
     try {
 
-        let curruser1 = await User.findById(req.params.id).lean();
+
+        let curruser1 = await User.findById(req.user._id).lean();
+        console.log(curruser1);
 
         curruser1.firstName = req.body.firstName;
         curruser1.lastName = req.body.lastName;
         curruser1.email = req.body.email;
         curruser1.displayName = curruser1.firstName + " " + curruser1.lastName
 
-        console.log(curruser1.firstName + "this is the first name");
-        console.log(curruser1.lastName + "this is the last name");
-        console.log(curruser1.displayName + "this is the disply name");
-        console.log(curruser1.email + "this is the email");
-        curruser1 = await User.findOneAndUpdate({ _id: req.params.id }, {
+        curruser1 = await User.findOneAndUpdate({ _id: curruser1._id }, {
             firstName: curruser1.firstName,
             lastName: curruser1.lastName,
             email: curruser1.email,
@@ -58,6 +53,7 @@ router.put("/:id", ensureAuth, async(req, res) => {
             new: true, // it will create a new one, if it doesn't exist
             runValidators: true, // it check weather the fields are valid or not
         });
+
         res.redirect("/portfolio");
     } catch (err) {
         console.error(err)
